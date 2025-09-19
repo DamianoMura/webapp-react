6ecbbd2725e6ffd62f202c2a0e75bdcac44cdcde
DamianoMura <mura.damiano14@gmail.com>
2025-09-19 20:14:45 +0200

    [upd] hover effect that leads to detail page

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import DefaultLayout from "./components/layout_components/DefaultLayout"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Movies from './pages/Movies';
+import MovieDetail from './pages/MovieDetail'
 function App() {
-  
-
+    
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<DefaultLayout/>}>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
-          <Route path="/movies" element={<Movies/>}/>
+          <Route path="/movies" >
+            <Route index element={<Movies/>}/>
+            <Route path=":id" element={<MovieDetail/>}/>
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,6 +1,7 @@
 const endpoint="http://localhost:3000/movies/";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Movies = () => {
   const [movies,setMovies]=useState([]);
@@ -9,15 +10,39 @@ const Movies = () => {
     
       axios.get(endpoint).then((resp)=>{
          setMovies(resp.data)
-        
+        console.log(movies)
       })
   },[])
   return (
    <div className="container">
-    <div className="row">
+    <div className="row g-2">
       <div className="col-12">
         <h1 className="text-center">Movies List</h1>
       </div>
+      {
+        movies.map((movie)=>{
+          return(
+            <div className="col-12 col-md-6 col-lg-4" key={movie.id}>
+              <div className="card">
+                <div className="card-header text-center">
+                  <h3>{movie.title}</h3>
+                </div>
+                <div className="card-body ">
+                    <div className="hovered">
+                      <Link to={`/movies/${movie.id}`} className="btn btn-primary" >Vedi dettaglio</Link>
+                    </div>
+                  
+                  <div className="image-frame">
+                    <img src={movie.image} alt={movie.title} />
+                  <span>Directed by : {movie.director}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          )
+        })
+      }
     </div>
    </div>
   )
