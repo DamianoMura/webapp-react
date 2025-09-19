d7b7bb0f4b0285b8597c91adce4b8b3336fdbbd3
DamianoMura <mura.damiano14@gmail.com>
2025-09-19 16:28:50 +0200

    [add] routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-
-
-
+import { BrowserRouter, Router, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
 function App() {
   
 
   return (
-    <>
-      <h1>hello Booleaners!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+          
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
 
